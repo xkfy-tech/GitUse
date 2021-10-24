bf994a4fd97fc9e39c1192cd4c3f8c78c089286e
xkfy-tech <285956669@qq.com>
2021-10-24 17:01:17 +0800

    i + 20;

--- app/src/main/java/com/gly/gituse/MainActivity.java
+++ app/src/main/java/com/gly/gituse/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        i = i + 1;
+        i = i + 20;
         j = j + 1;
         k = k + 1;
     }
